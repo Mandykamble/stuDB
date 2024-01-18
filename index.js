80f4e3b9558cf893e96f0f005e2dd88e1454e849
Mandykamble <99462698+Mandykamble@users.noreply.github.com>
2024-01-18 15:26:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ mongoose.connect(mongoDBURI);
 console.log(mongoose.connection.readyState);
 
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 
 // const getUser = () => undefined;
